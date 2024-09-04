fa0404f26499c1d738d706ccf5a682a9e75846ab
km0programming <73245809+km0programming@users.noreply.github.com>
2024-09-04 20:52:02 +0200

    setup

---
+++ test.py
@@ -0,0 +1,30 @@
+import os
+from openai import OpenAI
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+# print(OPENAI_API_KEY)
+
+import openai
+
+# print(openai.__version__)
+
+
+from openai import OpenAI
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+completion = client.chat.completions.create(
+#   model="gpt-4o-mini-2024-07-18",
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    # {"role": "user", "content": "Please list the most important newspapers in Spain?"}
+    {"role": "user", "content": "the newspaper 'El Español' from which city is it?"}
+  ]
+)
+
+print(completion.choices[0].message.content)
+
+
+
+
