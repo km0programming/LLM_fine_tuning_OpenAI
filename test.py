4c4f829ee82a30b64f6d6d8e6269e4616ca7eabc
km0programming <73245809+km0programming@users.noreply.github.com>
2024-09-10 09:01:16 +0200

    aa

--- test.py
+++ test.py
@@ -12,14 +12,30 @@ import openai
 # print(openai.__version__)
 
 
+  # Define the system prompt
+system_prompt = "Given the medical description report, classify it into one of these categories: " + \
+              "[Cardiovascular / Pulmonary, Gastroenterology, Neurology, Radiology, Surgery]"
+
 from openai import OpenAI
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+# completion = client.chat.completions.create(
+# #   model="gpt-4o-mini-2024-07-18",
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     # {"role": "user", "content": "Please list the most important newspapers in Spain?"}
+#     {"role": "user", "content": "Tell me something about Mercedes-Benz'?"}
+#   ]
+# )
+
+
 
 completion = client.chat.completions.create(
 #   model="gpt-4o-mini-2024-07-18",
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "system", "content": system_prompt},
     # {"role": "user", "content": "Please list the most important newspapers in Spain?"}
     {"role": "user", "content": "Tell me something about Mercedes-Benz'?"}
   ]
